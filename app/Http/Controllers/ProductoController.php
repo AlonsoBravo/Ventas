aa8aa60478f584d5688d3edf19415e4d174cccd5
Alonso Bravo <42778327+AlonsoBravo@users.noreply.github.com>
2018-12-04 20:35:12 -0300

    cambio
    cambio

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -15,4 +15,23 @@ class ProductoController extends Controller
         $familiaProductos = FamiliaProducto::all();
         return view ('productos.productos', compact('productos','familiaProductos'));
     }
+
+    public function crearNuevoProducto(Request $request){
+
+        $familia = $request->get('lista_producto');
+
+
+        DB::table('productos')->insert([
+            'codigo_producto'=>$request->codProducto,
+            'codigo_familia' =>$request->lista_producto,
+            'rut_proveedor' =>$request->rutProveedor,
+            'nombre_producto'=>$request->nomProducto,
+            'valor_unitario'=>$request->valUnitario,
+            'stock'=>$request->stock,
+            'stock_minimo'=>$request->stockMin,
+            'stock_maximo'=>$request->stockMax,
+        ]);
+
+        return redirect()->route('productos')->with('mensaje','ok');
+    }
 }
